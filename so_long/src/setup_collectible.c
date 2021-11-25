b1b9fa13b3f9f9f068c3b28c07a588085639be86
MarJC5 <admin@marjc.ch>
2021-11-25 01:09:44 +0100

    Update

--- so_long/src/setup_collectible.c
+++ so_long/src/setup_collectible.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 13:24:26 by jmartin           #+#    #+#             */
-/*   Updated: 2021/11/25 01:02:34 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/11/25 01:07:25 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,9 @@ int	is_coins_taken(int key, int position, t_view *view)
 	return (1);
 }
 
-int	open_door(t_view *view)
+int	open_door(t_view *view, int key)
 {
+	(void) key;
 	set_static_items(view, "./img/xpm/tiles/1.1.xpm", 'E');
 	set_static_items(view, "./img/xpm/exit/E.1.xpm", 'E');
 	return (1);
